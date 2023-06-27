8d47554804f4411a3dea640a7f980c01a630e67a
Akushkina Kseniya <akushkina.ka@yandex.ru>
2023-06-28 01:45:52 +0300

    Sprint 15: add some tests

--- src/main/java/ru/practicum/shareit/item/dto/ItemDtoResponse.java
+++ src/main/java/ru/practicum/shareit/item/dto/ItemDtoResponse.java
@@ -1,12 +1,12 @@
 package ru.practicum.shareit.item.dto;
 
 import lombok.Builder;
-import lombok.Data;
+import lombok.Getter;
 import ru.practicum.shareit.user.dto.UserDtoRequest;
 
 import java.util.List;
 
-@Data
+@Getter
 @Builder
 public class ItemDtoResponse {
     private long id;
--- src/main/java/ru/practicum/shareit/request/dto/RequestDtoResponse.java
+++ src/main/java/ru/practicum/shareit/request/dto/RequestDtoResponse.java
@@ -1,14 +1,14 @@
 package ru.practicum.shareit.request.dto;
 
 import lombok.Builder;
-import lombok.Data;
+import lombok.Getter;
 import ru.practicum.shareit.item.dto.ItemDtoResponse;
 import ru.practicum.shareit.user.dto.UserDtoRequest;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
-@Data
+@Getter
 @Builder
 public class RequestDtoResponse {
     private long id;
